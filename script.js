bf0e6cda1f651136f53682d208e7b03798ef82a1
myeon504 <80014413+myeon504@users.noreply.github.com>
2023-11-01 03:13:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -615,3 +615,78 @@ navNext.addEventListener('click', function(e){
 });
 
 goToSlide(0);
+
+window.onload = function () { buildCalendar(); }   
+
+let nowMonth = new Date();  
+let today = new Date();    
+today.setHours(0, 0, 0, 0);    
+
+
+function buildCalendar() {
+
+    let firstDate = new Date(nowMonth.getFullYear(), nowMonth.getMonth(), 1);     
+    let lastDate = new Date(nowMonth.getFullYear(), nowMonth.getMonth() + 1, 0);  
+
+    let tbody_Calendar = document.querySelector(".calendar > tbody");
+    document.getElementById("calYear").innerText = nowMonth.getFullYear();            
+    document.getElementById("calMonth").innerText = leftPad(nowMonth.getMonth() + 1);  
+
+    while (tbody_Calendar.rows.length > 0) {                        
+        tbody_Calendar.deleteRow(tbody_Calendar.rows.length - 1);
+    }
+
+    let nowRow = tbody_Calendar.insertRow();                
+
+    for (let j = 0; j < firstDate.getDay(); j++) {  
+        let nowColumn = nowRow.insertCell();        
+    }
+
+    for (let nowDay = firstDate; nowDay <= lastDate; nowDay.setDate(nowDay.getDate() + 1)) {     
+
+        let nowColumn = nowRow.insertCell();       
+
+
+        let newDIV = document.createElement("p");
+        newDIV.innerHTML = leftPad(nowDay.getDate());       
+        nowColumn.appendChild(newDIV);
+
+        if (nowDay.getDay() == 6) {                
+            nowRow = tbody_Calendar.insertRow();   
+        }
+
+        if (nowDay < today) {                      
+            newDIV.className = "pastDay";
+        }
+        else if (nowDay.getFullYear() == today.getFullYear() && nowDay.getMonth() == today.getMonth() && nowDay.getDate() == today.getDate()) { // 오늘인 경우           
+            newDIV.className = "today";
+            newDIV.onclick = function () { choiceDate(this); }
+        }
+        else {                                      
+            newDIV.className = "futureDay";
+            newDIV.onclick = function () { choiceDate(this); }
+        }
+    }
+}
+
+// 날짜 선택
+function choiceDate(newDIV) {
+    if (document.getElementsByClassName("choiceDay")[0]) {                              // 기존에 선택한 날짜가 있으면
+        document.getElementsByClassName("choiceDay")[0].classList.remove("choiceDay");  // 해당 날짜의 "choiceDay" class 제거
+    }
+    newDIV.classList.add("choiceDay");           // 선택된 날짜에 "choiceDay" class 추가
+}
+
+function prevCalendar() {
+    nowMonth = new Date(nowMonth.getFullYear(), nowMonth.getMonth() - 1, nowMonth.getDate());   
+    buildCalendar();    
+}
+
+function nextCalendar() {
+    nowMonth = new Date(nowMonth.getFullYear(), nowMonth.getMonth() + 1, nowMonth.getDate());   
+    buildCalendar();    
+}
+
+function leftPad(value) {
+    return value;
+}
